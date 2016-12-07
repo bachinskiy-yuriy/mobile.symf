0d58d68930474fc97fd5ed0de99ab449178f2d08
Bachinskiy Yuriy <bachinskiy.yuriy@gmail.com>
2016-12-07 20:14:20 +0200

    Mobile.Symf ver.1.0.0
    only symfony installed

---
+++ README.md
@@ -0,0 +1,4 @@
+mobile.symf
+===========
+
+A Symfony project created on December 7, 2016, 7:01 pm.
---
+++ _runAllServices.cmd
@@ -0,0 +1,4 @@
+start d:\wamp\wampmanager.exe
+start php app/console server:run
+start "" http://localhost:8000/main
+start "" http://localhost/phpmyadmin/
